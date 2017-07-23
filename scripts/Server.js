cd3558e6983adfd6b7c6ec6c186b2570635f11c2
abuac13 <abuac13@freeuni.edu.ge>
2017-07-23 14:14:13 +0400

    Add corection

--- scripts/Server.js
+++ scripts/Server.js
@@ -7,6 +7,7 @@ app.use('/css', express.static(__dirname + '/../css'));
 app.use('/scripts', express.static(__dirname));
 app.use('/images', express.static(__dirname + '/../images'));
 app.use('/libs', express.static(__dirname + '/../libs'));
+app.use('/menu', express.static(__dirname + '/../menu'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../index.html'));
