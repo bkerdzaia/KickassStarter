fe0ea0b0d53d3c0874a783515ee6ace6e4e1df66
abuac13 <abuac13@freeuni.edu.ge>
2017-07-23 19:35:32 +0400

    Add new user after signup

--- scripts/Server.js
+++ scripts/Server.js
@@ -2,9 +2,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-//var mongoose = require('mongoose');
-//require('./DBconnection');
-//var User = mongoose.model('User');
+var mongoose = require('mongoose');
+require('./DBconnection');
+var User = mongoose.model('User');
 var bodyParser = require('body-parser');
 
 app.use('/', express.static(__dirname + '/../'));
@@ -13,30 +13,30 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../index.html'));
 });
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signup', function (req, res) {
-    /*var uname = req.body.username;
-    var umail = req.body.email;
-    var upassword = req.body.password;
+var user;
 
-    var user = new User({
-        username: uname,
-        password: upassword,
-        email: umail,
+app.post('/signup', function (req, res) {
+
+    user = new User({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
         photo: '../images/avatar.jpg',
-        logedin: true
+        logedIn: true
     });
+
+    console.log(user);
 
     user.save(function (err) {
         if(err)
-            console.log("user saving error");
+            console.log("user saving error", err);
         else
             console.log("user successfuly saved");
     });
 
-    res.sendFile(path.join(__dirname + '../index.html'));*/
+    res.sendFile(path.join(__dirname + '/../index.html'));
 	console.log("requ: ", req.body);
 
 });
