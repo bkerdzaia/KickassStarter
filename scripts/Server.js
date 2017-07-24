6cb1482fa1d912bf2e357722d62114a7d04615d9
abuac13 <abuac13@freeuni.edu.ge>
2017-07-25 02:32:41 +0400

    minor updates

--- scripts/Server.js
+++ scripts/Server.js
@@ -1,4 +1,6 @@
 
+import {request} from "../../../../../../../../home/koka/.WebStorm2017.2/config/javascript/extLibs/http_github.com_DefinitelyTyped_DefinitelyTyped_raw_master_types_express-serve-static-core_index";
+
 require('./DBconnection');
 var express = require('express');
 var mongoose = require('mongoose');
@@ -7,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 var User = mongoose.model('User');
+var Project = mongoose.model('Project');
 
 mongoose.Promise = global.Promise;
 app.use('/', express.static(__dirname + '/../'));
@@ -17,6 +20,8 @@ app.get('/', function(req, res) {
 });
 
 var user;
+var project;
+
 app.post('/signup', function (req, res) {
 
     user = new User({
@@ -26,8 +31,6 @@ app.post('/signup', function (req, res) {
         photo: '../images/avatar.jpg',
         logedIn: true
     });
-
-    console.log(user);
 
     user.save(function (err) {
         if(err)
@@ -53,7 +56,71 @@ app.post('/login', function (req, res) {
                     console.log("user successfuly saved");
             });
 
+            this.user = user;
+
             res.sendFile(path.join(__dirname + '/../index.html'));
+        }
+    });
+
+});
+
+app.post('/projectAdd', function (req, res) {
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    project = new Project({
+        name: req.body.name,
+        description: req.body.description,
+        photo: req.body.photo,
+        category: req.body.category,
+        created_at: date + " " + time,
+        enddate: req.body.enddate,
+        targetMoney:req.body.money,
+        budget: 0,
+        info: req.body.content,
+        author: req.body.couserId,
+        sharesPercenage: req.body.sharespercentage,
+        numvisits: 0
+    });
+
+    project.save(function (err) {
+        if(err)
+            console.log("project saving error", err);
+        else
+            console.log("project successfuly saved");
+    });
+
+    res.sendFile(path.join(__dirname + '/../startproject.html'));
+
+});
+
+app.get('projectsList', function (req, res) {
+
+    Project.find(function (err, projectlist) {
+        if(err) console.log("Can't get projects list");
+        else{
+            var projectsjson = {
+                projectlists : []
+            };
+
+            for(var i=0; i<projectlist.length; i++){
+                var proj = projectlist[i];
+                var uid = proj.author;
+                var us = User.find({_id: uid});
+                projectsjson.projectlists.push({
+                    category: proj.category,
+                    name: proj.name,
+                    content: proj.info,
+                    image: proj.photo,
+                    owner: {
+                        uId: uid,
+                        name: us.name,
+                        avatar: us.photo
+                    }
+                });
+            }
+
+            res.send(projectsjson);
         }
     });
 
