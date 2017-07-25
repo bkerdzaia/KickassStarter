1e516060e2186fc8f7cea13571dc238ee27ee808
bkerdzaia <b.kerdzaia@gmail.com>
2017-07-25 17:23:29 +0400

    correct typos add logout"

--- scripts/locStorage.js
+++ scripts/locStorage.js
@@ -4,6 +4,30 @@ function sendId(id) {
         document.getElementById("um_login").href = "profsettings";
         document.getElementById("um_sign").innerHTML = "Logout";
         document.getElementById("um_sign").href = "logout";
+        document.getElementById("um_sign").addEventListener('click', function(event){
+            server.sendRequest('logout', function(res){});
+            document.getElementById('umenu').innerHTML = `
+
+                <a href="#">
+                    <img src="./images/kaslogo.png">
+                </a>
+                <a href="#">
+                    Home
+                </a>
+                <a href="#/explore">
+                    Explore
+                </a>
+                <a id="um_login" href="login" data-navigo>
+                    Login
+                </a>
+                <a id="um_sign" href="signup" data-navigo>
+                    Sign Up
+                </a>
+
+            `;
+            localStorage.removeItem('userId');
+            router.navigate('');
+        });
         document.getElementById("myprofile").innerHTML = "My Profile";
         document.getElementById("myprofile").href = "profile";
     });
